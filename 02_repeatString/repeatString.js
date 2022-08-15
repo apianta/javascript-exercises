144c5943ae571fb5432189e77e3057641327f4ce
Andrew Pianta <andrewpianta@andrew-pianta-trakstar.attlocal.net>
2022-08-15 10:26:11 -0400

    Finish removeFromArray exercise.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+	str = "hey";
+	if (num > 0) {
+		return str.repeat(num);
+	} else if (num < 0) {
+		return "ERROR";
+	} else {
+		return "";
+	}
 };
-
+repeatString("hey", 3);
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+	return str === "" ? "" : reverseString(str.substr(1)) + str.charAt(0);
 };
+
+reverseString("hello there");
 
 // Do not edit below this line
 module.exports = reverseString;
